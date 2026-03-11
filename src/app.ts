89dc49c325efadb37514ab8eef364e07c38f0776
srinivas-elimilla <srinivaselimilla1@gmail.com>
2026-03-11 06:57:14 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors"
 import { promptRouter } from "./routes/prompt.routes";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
